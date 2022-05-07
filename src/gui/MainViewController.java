c5176426a00de8d547fea705918666add6dc10e7
alguitti <andre.guitti@yahoo.com.br>
2022-05-07 18:25:24 -0300

    DepartmentList View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -32,7 +32,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("MenuItem Department");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
